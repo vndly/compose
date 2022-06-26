58d2c769a2121880b0f4c7e41f56a0f083ca1048
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-06-26 23:38:53 +0200

    Improvements

--- app/src/main/java/com/mauriciotogneri/composetest/image/activity/ImageActivity.kt
+++ app/src/main/java/com/mauriciotogneri/composetest/image/activity/ImageActivity.kt
@@ -3,8 +3,6 @@ package com.mauriciotogneri.composetest.image.activity
 import android.content.Context
 import android.content.Intent
 import com.mauriciotogneri.composetest.base.BaseActivity
-import com.mauriciotogneri.composetest.counter.state.CounterState
-import com.mauriciotogneri.composetest.counter.view.CounterView
 import com.mauriciotogneri.composetest.image.state.ImageState
 import com.mauriciotogneri.composetest.image.view.ImageView
 
