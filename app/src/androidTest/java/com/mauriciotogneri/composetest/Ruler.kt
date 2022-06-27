6be308602b82a0887d6c711d42b52a246d505643
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-06-27 22:38:10 +0200

    Improvements

--- app/src/androidTest/java/com/mauriciotogneri/composetest/Ruler.kt
+++
@@ -1,9 +0,0 @@
-package com.mauriciotogneri.composetest
-
-import androidx.compose.ui.test.junit4.ComposeContentTestRule
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import com.mauriciotogneri.composetest.counter.activity.CounterActivity
-
-/*object Ruler {
-    val rule: ComposeContentTestRule = createAndroidComposeRule<CounterActivity>()
-}*/
