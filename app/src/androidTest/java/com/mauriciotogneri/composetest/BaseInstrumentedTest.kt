eb43df487e883841f3525c259ce723c5ad34c521
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-06-27 20:41:17 +0200

    Improvements

--- app/src/androidTest/java/com/mauriciotogneri/composetest/BaseInstrumentedTest.kt
+++ app/src/androidTest/java/com/mauriciotogneri/composetest/BaseInstrumentedTest.kt
@@ -2,9 +2,12 @@ package com.mauriciotogneri.composetest
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -13,4 +16,9 @@ abstract class BaseInstrumentedTest<A : ComponentActivity> {
     val compose: AndroidComposeTestRule<ActivityScenarioRule<A>, A> by lazy { rule() }
 
     abstract fun rule() : AndroidComposeTestRule<ActivityScenarioRule<A>, A>
+
+    abstract fun execute()
+
+    @Test
+    fun runTests() = execute()
 }
--- app/src/androidTest/java/com/mauriciotogneri/composetest/InstrumentedTest.kt
+++ app/src/androidTest/java/com/mauriciotogneri/composetest/InstrumentedTest.kt
@@ -5,21 +5,13 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.mauriciotogneri.composetest.counter.activity.CounterActivity
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class InstrumentedTest : BaseInstrumentedTest<CounterActivity>() {
     override fun rule() = createAndroidComposeRule<CounterActivity>()
 
-    @Test
-    fun openScreen1() {
-        compose.onNodeWithText("Clicked 0 times").performClick()
-        compose.onNodeWithText("Clicked 1 times").assertExists()
-    }
-
-    @Test
-    fun openScreen2() {
+    override fun execute() {
         compose.onNodeWithText("Clicked 0 times").performClick()
         compose.onNodeWithText("Clicked 1 times").performClick()
         compose.onNodeWithText("Clicked 2 times").assertExists()
