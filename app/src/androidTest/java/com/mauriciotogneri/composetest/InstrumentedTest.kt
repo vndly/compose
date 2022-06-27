74a22f4c9018ec9ee3984a3dff17fbc54f5d655a
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-06-27 20:18:57 +0200

    Improvements

--- app/src/androidTest/java/com/mauriciotogneri/composetest/InstrumentedTest.kt
+++ app/src/androidTest/java/com/mauriciotogneri/composetest/InstrumentedTest.kt
@@ -1,25 +1,27 @@
 package com.mauriciotogneri.composetest
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.mauriciotogneri.composetest.counter.activity.CounterActivity
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class InstrumentedTest {
-    @Test
-    fun useAppContext() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mauriciotogneri.composetest", appContext.packageName)
-    }
+    @get:Rule
+    val compose = createComposeRule()
 
     @Test
     fun openScreen() {
         ActivityScenario.launch(CounterActivity::class.java)
 
-        //composeTestRule.onNode().performClick()
+        compose.onNodeWithText("Clicked 0 times").performClick()
+        compose.onNodeWithText("Clicked 1 times").assertExists()
     }
 }
