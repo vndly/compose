e72fe7bf4fc562374d1e2b43c69568c1b0e4fcbe
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-07-01 20:52:35 +0200

    Improvements

--- app/src/main/java/com/mauriciotogneri/composetest/counter/activity/CounterActivity.kt
+++ app/src/main/java/com/mauriciotogneri/composetest/counter/activity/CounterActivity.kt
@@ -1,16 +1,24 @@
 package com.mauriciotogneri.composetest.counter.activity
 
+import android.os.Bundle
 import com.mauriciotogneri.composetest.base.BaseActivity
 import com.mauriciotogneri.composetest.counter.state.CounterState
-import com.mauriciotogneri.composetest.counter.state.CounterStateCallback
 import com.mauriciotogneri.composetest.counter.state.CounterStateFactory
 import com.mauriciotogneri.composetest.counter.view.CounterView
 import com.mauriciotogneri.composetest.image.activity.ImageActivity
 
-class CounterActivity : BaseActivity<CounterState, CounterView>(), CounterStateCallback {
-    override fun createState() = CounterStateFactory(this, this).get()
+class CounterActivity : BaseActivity<CounterState, CounterView>() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        state.onOpenScreen.observe(this) { url ->
+            startActivity(ImageActivity.intent(url, this))
+        }
+    }
+
+    override fun createState() = CounterStateFactory(this).get()
 
     override fun createView() = CounterView(state)
 
-    override fun onOpenNewScreen(url: String) = startActivity(ImageActivity.intent(url, this))
+    fun onOpenNewScreen(url: String) = startActivity(ImageActivity.intent(url, this))
 }
--- app/src/main/java/com/mauriciotogneri/composetest/counter/state/CounterState.kt
+++ app/src/main/java/com/mauriciotogneri/composetest/counter/state/CounterState.kt
@@ -3,32 +3,27 @@ package com.mauriciotogneri.composetest.counter.state
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
 import com.mauriciotogneri.composetest.base.BaseState
+import com.mauriciotogneri.composetest.base.Event
 import com.mauriciotogneri.composetest.counter.model.Counter
 
-class CounterState(private val callback: CounterStateCallback) : BaseState() {
+class CounterState : BaseState() {
     var counter by mutableStateOf(Counter())
+
+    val onOpenScreen = MutableLiveData<String>()
 
     fun increaseCounter() {
         counter = Counter(counter.value + 1)
     }
 
-    fun openNewScreen() = callback.onOpenNewScreen("https://foo.com/image")
+    fun openNewScreen() = onOpenScreen.postValue("https://foo.com/image")
 }
 
 class CounterStateFactory(
     private val lifecycle: ViewModelStoreOwner,
-    private val callback: CounterStateCallback,
-) : ViewModelProvider.Factory {
-    @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T = CounterState(callback) as T
-
-    fun get() = ViewModelProvider(lifecycle, this).get(CounterState::class.java)
-}
-
-interface CounterStateCallback {
-    fun onOpenNewScreen(url: String)
+) {
+    fun get() = ViewModelProvider(lifecycle).get(CounterState::class.java)
 }
