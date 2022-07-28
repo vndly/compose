f55e9ec67f22c3ac9fa7fc218f63052564122c66
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-07-28 20:09:23 +0200

    Improvements

--- app/src/main/java/com/mauriciotogneri/composetest/api/TodoApi.kt
+++ app/src/main/java/com/mauriciotogneri/composetest/api/TodoApi.kt
@@ -4,12 +4,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object TodoApi {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com"
+
     val getTodos: GetTodosEndpoint by lazy {
+        service(GetTodosEndpoint::class.java)
+    }
+
+    private fun <T> service(service: Class<T>): T =
         Retrofit
             .Builder()
-            .baseUrl("https://jsonplaceholder.typicode.com")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(GetTodosEndpoint::class.java)
-    }
+            .create(service)
 }
